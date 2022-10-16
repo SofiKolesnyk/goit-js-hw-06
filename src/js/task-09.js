44cb64a2d40e48d552039e8a901a5b90dc547191
SofiKolesnyk <107218240+SofiKolesnyk@users.noreply.github.com>
2022-10-16 18:35:22 +0300

    Add files via upload

---
+++ src/js/task-09.js
@@ -0,0 +1,21 @@
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+
+const btnRef = document.querySelector('.change-color');
+
+const body = document.querySelector('body');
+
+const codeColorRef = document.querySelector('.color');
+
+btnRef.addEventListener('click', () => getRandomHexColor());
+
+function getRandomHexColor() {
+  const newColor = "#" + Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0);
+  console.log(newColor);
+  body.style.background = newColor
+  return newColor
+}
+console.log(btnRef);
